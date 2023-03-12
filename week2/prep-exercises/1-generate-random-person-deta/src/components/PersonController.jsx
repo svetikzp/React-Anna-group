faaf2e2ddb72431e5168dcb0877b4d8516097273
svetikzp <s.ostryanskaya@gmail.com>
2023-03-13 00:57:10 +0100

    add files

--- week2/prep-exercises/1-generate-random-person-deta/src/components/PersonController.jsx
+++ week2/prep-exercises/1-generate-random-person-deta/src/components/PersonController.jsx
@@ -8,10 +8,10 @@ export default function PersonController() {
    React.useEffect(() => { getPerson() }, []);
 
    async function getPerson() {
-      // fetch('https://www.randomuser.me/api?results=2')
-      //    .then((response) => response.json())
-      //    .then((person) => { setPerson(person) })
-      // const response = await fetch('https://www.randomuser.me/api?results=2');
+       //fetch('https://www.randomuser.me/api?results=2')
+          //.then((response) => response.json())
+          //.then((person) => { setPerson(person) })
+       //const response = await fetch('https://www.randomuser.me/api?results=2');
       // const person = await response.json();
       const person = await (await fetch('https://www.randomuser.me/api?results=2')).json();
       // cons
