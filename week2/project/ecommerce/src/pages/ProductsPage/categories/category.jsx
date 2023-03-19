02b9989c0d40bd440671c509220fdcf4ca72b4f3
svetikzp <s.ostryanskaya@gmail.com>
2023-03-19 17:30:08 +0100

    del

--- week2/project/ecommerce/src/pages/ProductsPage/categories/category.jsx
+++
@@ -1,6 +0,0 @@
-export default function Category({ category, onClick, activeCategory }) {
-
-    const className = (category === activeCategory) ? 'button active' : 'button'
- 
-    return <button className={className} onClick={() => { onClick(category) }}>{category}</button>
- }
