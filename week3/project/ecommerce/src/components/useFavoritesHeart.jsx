dfef35a05db869cd0ec38747b262c0d3eab8cd82
svetikzp <s.ostryanskaya@gmail.com>
2023-03-28 21:46:30 +0200

    add

--- week3/project/ecommerce/src/components/useFavoritesHeart.jsx
+++ week3/project/ecommerce/src/components/useFavoritesHeart.jsx
@@ -1,34 +1,8 @@
 import { useContext } from 'react'
 import { FavoriteContext } from '../context/FavoriteContext'
-import regHeart from '../../heart-regular.svg'
-import solHeart from '../../../assets/heart-solid.svg'
+import regHeart from '../assets/heart-regular.svg'
+import solHeart from '../assets/heart-solid.svg'
 	
-	/*export default function useFavoritesHeard(itemId) {
-	
-	   const { idFavoriteItems, setIdFavoriteItems } = useContext(FavoriteContext)
-	   const [svgIcon, setSvgItem] = useState(() => idFavoriteItems.includes(itemId) ? svgSol : svgReg)
-	
-	  // eslint-disable-next-line
-	   useEffect(() => { idFavoriteItems.includes(itemId) ? setSvgItem(svgSol) : setSvgItem(svgReg) }, [itemId])
-	
-	
-	   function favoriteIdHandler(itemId) {
-	      if (!idFavoriteItems.includes(itemId)) {
-	         setIdFavoriteItems((prev) => [...prev, itemId])
-	         setSvgItem(svgSol)
-	      }
-	      else {
-	         setIdFavoriteItems((prev) => prev.filter(id => id !== itemId))
-	         setSvgItem(svgReg)
-	      }
-	   }
-	
-	   return {
-	      svgIcon,
-	      favoriteIdHandler,
-	      idFavoriteItems
-	   }
-	}*/
 	const useFavoritesHeart = (id) => {
 		const productId = id.itemId;
 	  
